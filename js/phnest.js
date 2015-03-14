868543fe7bd30824afbda57a0eafe13b9d69d554
Ashwin kumar <ashwinkumark10@gmail.com>
2015-03-14 12:50:00 +0530

    fixing it after a long time

--- js/phnest.js
+++ js/phnest.js
@@ -1,9 +1,19 @@
 function phNest(){
     $(function () {
+		getChartOptions = function(data){
+		
+		var d = new Date();
+		d.setMonth(d.getMonth() - 1);
+		d = d.getTime();
+
         var options = {
-            chart: {
-                renderTo: 'chartcontainer'
-            },
+			chart: {
+				renderTo: 'chartcontainer',
+				defaultSeriesType: 'column'
+			},
+			scrollbar: {
+				enabled: true
+			},
             title: {
                 text: null
             },
@@ -14,45 +24,56 @@ function phNest(){
                 enabled: false
             },
             plotOptions: {
-                column: {
-                    pointPlacement: 'between'
-                }
-            },
+				column: {
+					pointPadding: 0.2,
+					borderWidth: 0.5
+				}
+			},
             xAxis: {
                 tickmarkPlacement: 'on',
-                type: 'datetime',
                 labels: {
-                    enabled: true
+                    enabled: true,
+					formatter: function () {
+						var day = new Date(this.value);
+						day = day.toDateString().split(" ").slice(1);
+						return day.join(" ");
+					}
                 },
                 minorTickLength: 0,
                 tickLength: 0,
                 gridLineWidth: 0,
                 lineWidth: 0,
                 minorGridLineWidth: 0,
-                lineColor: 'transparent'
+                lineColor: 'transparent',
+				min: d
             },
             yAxis: {
                 labels: {
                     enabled: false
                 },
-                title: {
-                    text: null
-                },
+                min: 0,
+				title: {
+					text: 'Total hunts / day'
+				},
                 gridLineWidth: 0
-            },
+            }, 
+			
+			
             series: [{
-                data: [],
-                type: 'column',
+				data: data,
                 cursor: 'pointer',
                 events: {
                     click: function(event) {
-                        $.getJSON('hunts/' + event.point.x + '.json', function(jsonpost) {
+						var day = new Date(event.point.x * 1);
+						day = day.getFullYear() + "-" + (parseInt(day.getMonth()) + 1) + "-" + day.getDate();
+						$.getJSON('hunts/' + day + '.json', function(jsonpost) {
+							
                             $('.content').empty();
                             $('.content').packery({
                                 gutter: 0
                             });
                             drawCards(event.point.x, jsonpost);
-                        }).error(function() {console.log('error');});
+                        }).error(function() {console.log('error');}); 
                     }
                 },
                 allowPointSelect: true,
@@ -81,28 +102,35 @@ function phNest(){
                 shadow: 0
             }
         };
+		return options;
+		};
 
 
 
         $.getJSON('hunts/meta.json', function(json) {
             json.sort(sortdata);
             var data = [];
-            for(eachobj in json){
-                options.series[0].data.push([[json[eachobj].day][0] * 1, [json[eachobj].hunts][0] * 1]);
+			
+			for(eachobj in json){
+				data.push([[json[eachobj].day][0] * 1, [json[eachobj].hunts][0] * 1]);
             }
-
-            var chart = new Highcharts.Chart(options);
+			
+			var options = getChartOptions(data);
+			var chart = new Highcharts.Chart(options);
+			
+			
             chart.series[0].data[json.length-1].select();
-
-            $.getJSON('hunts/' + json[json.length-1].day + '.json', function(jsonpost) {
+			
+			var day = new Date([json[eachobj].day][0] * 1);
+			day = day.getFullYear() + "-" + (parseInt(day.getMonth()) + 1) + "-" + day.getDate();
+	
+            $.getJSON('hunts/' + day + '.json', function(jsonpost) {
                 $('.content').empty();
                 $('.content').packery({
                     gutter: 0
                 });
-                drawCards(json[json.length-1].day, jsonpost);
+				drawCards(json[json.length-1].day, jsonpost);
             }).error(function() {console.log('error');});
-
-
         }).error(function() {console.log('error');});
         function sortdata(a,b) {
             if (a.day < b.day)
@@ -142,11 +170,13 @@ function phNest(){
             card.find('.post-tagline').text(post.post.tagline);
             card.find('.post-comments a').attr('href', "http://www.producthunt.co/" + post.post.ph_url).text(post.post.comments + " comments");
 
+			/*
             card.find('.ph-user').attr("href", "http://www.producthunt.co/" + post.user.id);
             card.find('.user-image').attr('src', post.user.image);
             card.find('.user-name').text(post.user.name);
             card.find('.user-tagline').text(post.user.tagline);
             card.find('.user-twitter a').attr('href', "https://twitter.com/" + post.user.twitter).text("@" + post.user.twitter);
+			*/
 
             card.appendTo('.content');
             $('.content').packery('appended',card);
